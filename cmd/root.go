cad2d1d6001dcbd32b921139ae7ad53d58f22f4b
Alireza Karampour <alireza.karampoor@gmail.com>
2025-09-09 01:41:20 +0330

    tested nats lib on nats cluster running in minikube

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
+	"fmt"
 	"os"
 
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	"github.com/spf13/cobra"
+)
+
+var (
+	Nats *string
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -14,6 +22,38 @@ var rootCmd = &cobra.Command{
 	Use:   "sms",
 	Short: "a minimal SMS gateway",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := nats.Connect(fmt.Sprintf("nats://%s", *Nats))
+		if err != nil {
+			return err
+		}
+		js, err := jetstream.New(conn)
+		if err != nil {
+			return err
+		}
+		jss, err := js.CreateOrUpdateStream(context.Background(), jetstream.StreamConfig{
+			Name:        "sms",
+			Description: "testing",
+			Subjects:    []string{"SmsRequest"},
+			Storage:     jetstream.FileStorage,
+		})
+		if err != nil {
+			return err
+		}
+		msg := nats.NewMsg("SmsRequest")
+		msg.Data = []byte("Hello")
+
+		ack, err := js.PublishMsg(context.Background(), msg)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("seq: %d\n", ack.Sequence)
+
+		rawMsg, err := jss.GetMsg(context.Background(), ack.Sequence, jetstream.WithGetMsgSubject("SmsRequest"))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("GotMsg: %s\n", string(rawMsg.Data))
+
 		return nil
 	},
 }
@@ -28,4 +68,9 @@ func Execute() {
 }
 
 func init() {
+	Nats = rootCmd.Flags().StringP("nats", "n", "", "nats url")
+	err := rootCmd.MarkFlagRequired("nats")
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,11 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nats.go v1.45.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
